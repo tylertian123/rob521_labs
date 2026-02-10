fixed-command-scaling

src/rob521_lab2/nodes/l2_follow_path.py
@@ -139,7 +139,7 @@ class PathFollower():
             t = np.linspace(0, CONTROL_HORIZON, self.horizon_timesteps + 1, endpoint=True)
             for opt in range(self.num_opts):
                 trajectory = np.zeros((self.horizon_timesteps + 1, 3))
-                vel, rot_vel = self.all_opts_scaled[opt]
+                vel, rot_vel = self.all_opts[opt]
                 if rot_vel == 0:  # moving straight
                     trajectory[:, 0] = startX + vel * t * np.sin(startTheta)
                     trajectory[:, 1] = startY - vel * t * np.cos(startTheta)